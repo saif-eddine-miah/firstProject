a089becf3789fc9aec5eca2e3c231c94debaac41
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-03-17 17:32:20 +0100

    Slideshow error

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function slideControls(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i = 0;
+  let slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1};
+  if (n < 1) {slideIndex = slides.length};
+    for (let i = 0; index < slides.length; index++) {
+     slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block"; 
+  }
+
 const burgerLinks = document.querySelector(".mobile-links")
 
 function showBurgerMenu() {
