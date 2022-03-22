329d5a79c960688fd0bba68fe925b43873d1bf39
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-03-22 09:55:19 +0100

    Revert "Fixed empire-ships.html w3c problems"
    This reverts commit e1dff9f47bc5be4e3707b73d1e8b339c3c5218c8.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const burgerLinks = document.querySelector(".mobile-links")
 
 function showBurgerMenu() {
+  burgerLinks.classList.toggle("show-links");
+  burgerLinks.classList.toggle("mobile-links");
+}
+
+function showBurgerMenu() {
   burgerLinks.classList.toggle("burger-positioning")
   burgerLinks.classList.toggle("show-links");
   burgerLinks.classList.toggle("mobile-links");
--- w3c.js
+++
@@ -1,7 +0,0 @@
-import { w3cHtmlValidator } from 'w3c-html-validator';
-
-const options = { filename: 'empire-ships.html' };
-
-w3cHtmlValidator.validate(options).then(w3cHtmlValidator.reporter);
-
-// console.log(w3cHtmlValidator.validate(options).then(w3cHtmlValidator.reporter))
