788268b47f63e8186f4914c32bb4e7a7025cc4b6
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-03-24 09:32:40 +0100

    Fixed initial display text Tie fighter

--- index.js
+++ index.js
@@ -134,7 +134,15 @@ function plusSlides(n) {
 }
 
 
-
+slides[slideIndexEmpire-1].style.display = "block"; 
+specPageDesc.textContent = empireShipsArray[0].description;
+specClass.textContent = empireShipsArray[0].classe;
+specConstruction.textContent = empireShipsArray[0].construction;
+specLongeur.textContent = empireShipsArray[0].length;  
+specLargeur.textContent = empireShipsArray[0].width;
+specSpeed.textContent = empireShipsArray[0].maxSpeed;
+specCost.textContent = empireShipsArray[0].cost;
+specPilot.src = empireShipsArray[0].pilot;
 function showSlides(n) {
   let i = 0;
   let slides = document.getElementsByClassName("slides");
